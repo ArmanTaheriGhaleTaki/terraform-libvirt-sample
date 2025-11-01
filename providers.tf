08a56705184d91464fc5ee4c8f4f6743c58f2dbf
sarangkheribm <124032429+sarangkheribm@users.noreply.github.com>
2025-11-02 00:46:06 +0530

    Update Terraform required version to 1.13.4

--- providers.tf
+++ providers.tf
@@ -1,5 +1,6 @@
 terraform {
-  required_version = "~> 1.10.5"
+#  required_version = "~> 1.10.5"
+  required_version = "1.13.4"
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
